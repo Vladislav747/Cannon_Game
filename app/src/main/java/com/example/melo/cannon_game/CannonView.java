e708f4044f7bcfd9e6b95967f950e1a1662060b5
Melo <zippo585@gmail.com>
2018-04-26 02:14:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/melo/cannon_game/CannonView.java
@@ -0,0 +1,27 @@
+package com.example.melo.cannon_game;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by Melo on 26.04.2018.
+ */
+
+public class CannonView extends SurfaceView {
+    public CannonView(Context context) {
+        super(context);
+    }
+
+    public CannonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CannonView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CannonView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
